a1cbd189107701a68a213993a95a17ba86b333d9
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-01-19 21:22:26 +0700

    footer and header

--- fe/src/components/molecules/Footer/index.js
+++ fe/src/components/molecules/Footer/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./footer.css";
+
+const Icon = ({ img }) => {
+  return (
+    <div className="icon-wrapper">
+      <img src={img} alt="icon" />
+    </div>
+  );
+};
 
 const Footer = () => {
   return (
     <div>
-      <p>Component Footer</p>
+      <div>
+        <div>
+          <img src="https://i.imgur.com/4ZQZQ9r.png" alt="logo" width={30} />
+        </div>
+        <div>
+          <Icon />
+          <Icon />
+          <Icon />
+          <Icon />
+          <Icon />
+        </div>
+      </div>
     </div>
   );
 };
--- fe/src/components/molecules/Header/index.js
+++ fe/src/components/molecules/Header/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div>
-      <p>Component Header</p>
+    <div className="header">
+      <div className="header_logo">
+        <img src="" alt="logo" />
+        <h2>Nasya Blog</h2>
+      </div>
+      <div className="header_nav">test</div>
     </div>
   );
 };
